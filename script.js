eaee338c2a3397d2bfa74bf5e5e050555d7b9ea5
sadeg-akbarian <sadeg.akbarian@gmail.com>
2023-11-12 17:36:59 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+*,
+::before,
+::after {
+  box-sizing: border-box;
+}
+
+:root {
+
+}
+
+html {
+  font-family:   ;
+  direction: ltr;
+  font-size:   ;
+}
+
+body {
+  margin: 0;
+  height:  ;
+  background-color: var(--page-background-color);
+  color: var(--page-text-color);
+} 
